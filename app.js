a4b01d86254a3b56bd42251cf75ffc9ace200cd1
Dmitriy <dmitriypruss21041984@gmail.com>
2021-10-16 11:23:11 +0300

    add error mw

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.use(express.json());
 
 app.use('/api', router);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Error 505! ${err}`);
+});
+
 module.exports = app;
